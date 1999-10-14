d32c7514ac76d268e10dfd6e1a44285e5e3dbb23
houghton <>
1999-10-14 11:08:21 +0000

    Added percent used to output.

--- src/scripts/df.pl
+++ src/scripts/df.pl
@@ -41,7 +41,7 @@ if( ! $real_df )
 
 open( DFOUT, "$real_df -k @ARGV |" );
 
-print "          Size          Used         Avail  (all values in KB)\n";
+print "          Size          Used             Avail  (all values in KB)\n";
 
 while( <DFOUT> )
   {
@@ -51,12 +51,17 @@ while( <DFOUT> )
 	$total = PrettyNum( $1 );
 	$used = PrettyNum( $2 );
 	$avail = PrettyNum( $3 );
-	printf ("  %12s  %12s  %12s  $4\n", $total, $used, $avail);
+	$percent = $2 * 100 / $1;
+        printf ("  %12s  %12s %2d%%  %12s  $4\n",
+                $total, $used, $percent, $avail, );
       }
   }
 
 #
 # $Log$
+# Revision 1.3  1999/10/14 11:08:21  houghton
+# Added percent used to output.
+#
 # Revision 1.2  1999/05/04 11:42:08  houghton
 # Bug-Fix: was not passing args to real df prog.
 #
